b22dc84e9f9e5ca88c24685261718133ed252b8f
Alex Mozingo <alexmozingo@Alexs-Air.localdomain>
2021-01-07 15:54:52 -0500

    again

---
+++ databasecode.sql
@@ -0,0 +1,18 @@
+CREATE TABLE production (
+    dateprod date() not null
+    firstname varchar(50) not null
+    lastname varchar(50) not null
+    discmodel varchar(50) not null
+    plastic varchar(50) not null
+    location varchar(50) not null
+    shift varchar(50) not null
+    line varchar(50) not null
+    firsts varchar(50) not null
+    seconds varchar(50) not null
+    regrind varchar(50) not null
+    waste varchar(50) not null
+
+    $sql = "INSERT INTO production (date, location, shift, line, firstname, lastname, discmodel, plastic, firsts, seconds, regrind, waste) VALUES ($date, $location, $shift, $line, $fname, $lname, $disc, $plastic, $firsts, $seconds, $regrind, $waste);";
+
+
+)

f69a3b460221aeb8e848c119b2e3a8846561633b
Alex Mozingo <alexmozingo@Alexs-Air.localdomain>
2021-01-07 15:00:59 -0500

    date and css

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,17 +1,5 @@
 <?php
 
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$location = $_POST['location'];
-$shift = $_POST['shift'];
-$line = $_POST['line'];
-$disc = $_POST['disc'];
-$plastic = $_POST['plastic'];
-$firsts = $_POST['firsts'];
-$seconds = $_POST['seconds'];
-$regrind = $_POST['regrind'];
-$waste = $_POST['waste'];
-
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPasswork = "Tuesday1";
--- includes/log.inc.php
+++ includes/log.inc.php
@@ -2,6 +2,18 @@
 
 include_once 'includes/dbh.inc.php';
 
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$location = $_POST['location'];
+$shift = $_POST['shift'];
+$line = $_POST['line'];
+$disc = $_POST['disc'];
+$plastic = $_POST['plastic'];
+$firsts = $_POST['firsts'];
+$seconds = $_POST['seconds'];
+$regrind = $_POST['regrind'];
+$waste = $_POST['waste'];
+
     $sql = "INSERT INTO production (date, location, shift, line, firstname, lastname, discmodel, plastic, firsts, seconds, regrind, waste) VALUES ($date, $location, $shift, $line, $fname, $lname, $disc, $plastic, $firsts, $seconds, $regrind, $waste);";
 
     mysqli_query($conn, $sql)
